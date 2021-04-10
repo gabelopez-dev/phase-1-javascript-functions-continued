356303762490d3549a7e2dfc24153784d4946ce7
Gabriel Lopez <gabelopez84@gmail.com>
2021-04-10 17:36:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code here
+function saturdayFun(typeOfFun="roller-skate") {
+    console.log(`"This Saturday, I want to${typeOfFun}!!"`);
+  } 
+  saturdayFun();
+  //=>"This Saturday, I want to roller-skate!"
+  saturdayFun("roller-skate");
